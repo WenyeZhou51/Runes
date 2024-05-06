d9593c14d13b4523bb6f4683b3dc3f47b5e07625
Wenye Zhou <wz51@rice.edu>
2024-05-06 10:46:24 +0800

    added singleton scene manager and level transition logic

---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private static SceneManagerScript instance;
+    public static SceneManagerScript Instance
+    {
+        get {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<SceneManagerScript>();
+                if (instance == null) {
+                    GameObject singleton = new GameObject("singleton");
+                    instance = singleton.AddComponent<SceneManagerScript>();
+                }
+
+            }
+            return instance;
+            
+                
+        }
+    
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+
+        }
+        else {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void NextScene() {
+        if (SceneManager.GetActiveScene().buildIndex != SceneManager.sceneCountInBuildSettings-1) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/TilemapTrigger.cs
+++ Assets/Scripts/TilemapTrigger.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneManager : MonoBehaviour
+public class TilemapTrigger : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) {
+            SceneManagerScript.Instance.NextScene();
+        }
     }
 
     // Update is called once per frame
